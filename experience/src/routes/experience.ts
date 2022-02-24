0f9faac798031577e65d6683467f2e5f260baa86
areeba-if <areeba.iftikhar1993@gmail.com>
2022-02-24 16:34:32 -0600

    skaffold updated and unnecessary services removed

--- experience/src/routes/experience.ts
+++
@@ -1,9 +0,0 @@
-import express, { NextFunction, Response, Request } from 'express';
-
-const router = express.Router();
-
-router.get('/api/skills', (req: Request, res: Response) => {
-    res.status(200).send('helloworld');
-});
-
-export { router as experienceRouter };
