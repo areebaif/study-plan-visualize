682deff291c714edf9c0a732f2cf8ecef78a0144
areeba-if <areeba.iftikhar1993@gmail.com>
2022-03-03 13:58:11 -0600

    auth service files split for refactor

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,55 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cookieSession from "cookie-session";
-import swaggerUi from "swagger-ui-express";
-import swaggerJSDoc from "swagger-jsdoc";
-
 // environemnt variables defined in kubernetes deployments
 import "dotenv/config.js";
 import keys from "./config/keys";
-import swaggerDocument from "../swagger/auth-api.json";
-import { authRouter } from "./routes/auth";
-import { errorHandler } from "./middlewares/errorHandler";
+import { app } from "./app";
+import { Express } from "express";
 
-const startServer = async () => {
-  const app = express();
-  app.set("trust proxy", true);
-
+const startServer = async (app: Express) => {
   // check environemnt variables
   if (!keys.DATABASE || !keys.JWT_KEY || !keys.NODE_ENV)
     throw new Error("environment variable not defined");
 
   const PORT = parseInt(`${keys.PORT}`) || 5000;
 
-  // middleware
-  app.use(bodyParser.json());
-
-  // set up JWT support
-  app.use(
-    cookieSession({
-      // JWT token is encrypted.
-      // Hence turn off built-in encryption support as other microservices might not be in node
-      signed: false,
-      name: "session",
-      secure: process.env.NODE_ENV === "production",
-    })
-  );
-  // api-documentation
-  app.use(
-    "/api/users/auth-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-  );
-
-  // routes
-  app.use(authRouter);
-
-  // error-handler
-  app.use(errorHandler);
-
   app.listen(PORT, () => {
     console.log(`auth service running on ${PORT}`);
   });
 };
 
-startServer();
+startServer(app);
