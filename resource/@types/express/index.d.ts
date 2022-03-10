03dbd8d92d382b8ac4de24f3fed65c7a2a343417
areeba-if <areeba.iftikhar1993@gmail.com>
2022-03-10 14:44:42 -0600

    removed logs

--- resource/@types/express/index.d.ts
+++ resource/@types/express/index.d.ts
@@ -1,11 +1,6 @@
 import { MongoClient, Db } from 'mongodb';
 import { UserPayload } from '../../src/middlewares/currentUser';
 
-// export interface UserPayload {
-//   id: string;
-//   email: string;
-// }
-
 declare global {
     namespace Express {
         interface Request {
