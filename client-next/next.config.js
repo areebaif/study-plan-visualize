ca2782b7d8875dcc0319a61fdff39020d5b6c487
areeba-if <areeba.iftikhar1993@gmail.com>
2022-04-26 15:03:18 -0500

    next file syncing config added so skaffold can detect change

--- client-next/next.config.js
+++ client-next/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+};
+/** this configuration ensures file changes sync */
+module.exports = {
+  nextConfig,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
+    return config;
+  },
+};
