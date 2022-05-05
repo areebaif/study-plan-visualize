7a0bfbdc080cfb591308b4e3a4ebf0f9b2a3df3b
areeba-if <areeba.iftikhar1993@gmail.com>
2022-05-05 17:09:12 -0500

    statemanagement is working now

--- client-next/pages/_app.tsx
+++ client-next/pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import App from "next/app";
+import { AppProps, AppInitialProps, AppContext } from "next/app";
+import { AuthApiReturnData } from "../types/types";
+import { fetchData } from "../api/fetchData";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  console.log("inside app", pageProps);
+  return (
+    <div>
+      <h1>header</h1>
+      <Component {...pageProps} />
+    </div>
+  );
+};
 
-export default MyApp
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const { req } = appContext.ctx;
+  const apiUrl =
+    // cross namespace communication in kubernetes requires this syntax to reach a pod in another namespace.
+    // We are trying to reach ingress-nginx-controller service in ingress-nginx namespace from our next.js service pod which is defined in default namespace.
+    "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser";
+
+  const appProps: AppInitialProps = { pageProps: {} };
+  // await App.getInitialProps(appContext) fills individual pageProps.
+  // so if we are in homepage. Homepage getInitialProps will be called and this will fill appProps.pageProps
+  // which is the same pageprops you see above.
+  const individualPageProps = await App.getInitialProps(appContext);
+  console.log("ind", individualPageProps);
+  // This is statemanagement at app level
+  const appState = await fetchData<AuthApiReturnData>(
+    apiUrl,
+    "GET",
+    undefined,
+    req?.headers.cookie
+  );
+
+  // We are doing below to combine app level state with individual page props/state
+  appProps.pageProps = { ...individualPageProps, ...appState };
+
+  return { ...appProps };
+};
+
+export default MyApp;
--- client-next/pages/index.tsx
+++ client-next/pages/index.tsx
@@ -7,17 +7,13 @@ import type {
 import { AuthApiReturnData } from "../types/types";
 import { fetchData } from "../api/fetchData";
 
-const Home: NextPage<{ data: AuthApiReturnData }> = ({ data }) => {
-  console.log("hello", data);
-
-  return data.currentUser ? <h1>signed in</h1> : <h1>signed out</h1>;
+const Home: NextPage<{ pageProps: AuthApiReturnData }> = ({ pageProps }) => {
+  console.log("main page!!", pageProps);
+  return pageProps?.currentUser ? <h1>signed in</h1> : <h1>signed out</h1>;
 };
 
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+Home.getInitialProps = async (context) => {
   const { req } = context;
-
   const apiUrl =
     // cross namespace communication in kubernetes requires this syntax to reach a pod in another namespace.
     // We are trying to reach ingress-nginx-controller service in ingress-nginx namespace from our next.js service pod which is defined in default namespace.
@@ -28,10 +24,10 @@ export const getServerSideProps: GetServerSideProps = async (
     apiUrl,
     "GET",
     undefined,
-    req.headers.cookie
+    req?.headers.cookie
   );
-
   // Pass data to the page via props
-  return { props: { data: response } };
+  console.log("indexpage", response);
+  return { pageProps: { ...response } };
 };
 export default Home;
